dadacd1ad0c626a8eb74021e9c98a1678958eb16
Matt Barbier <matt.barbier@os.uk>
2019-05-10 14:10:58 +0100

    added newer wpf mvvm demo

---
+++ MvvmDemo/MvvmDemo2/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace MvvmDemo2
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StudentViewControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            MvvmDemo2.ViewModel.StudentViewModel studentViewModelObject =
+               new MvvmDemo2.ViewModel.StudentViewModel();
+            studentViewModelObject.LoadStudents();
+
+            StudentViewControl.DataContext = studentViewModelObject;
+        }
+    }
+}
---
+++ MvvmDemo/MvvmDemo2/ViewModel/StudentViewModel.cs
@@ -0,0 +1,28 @@
+﻿
+using MvvmDemo2.Model;
+using System.Collections.ObjectModel;
+
+namespace MvvmDemo2.ViewModel
+{
+
+    public class StudentViewModel
+    {
+
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+
+            students.Add(new Student { FirstName = "Mark", LastName = "Allain" });
+            students.Add(new Student { FirstName = "Allen", LastName = "Brown" });
+            students.Add(new Student { FirstName = "Linda", LastName = "Hamerski" });
+
+            Students = students;
+        }
+    }
+}
